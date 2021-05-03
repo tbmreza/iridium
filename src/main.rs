b3aad1c5c8b433473b1cbc768646c9a09fbf0c3d
Reza Handzalah <reza.handzalah@mbiz.co.id>
2021-05-03 16:00:29 +0700

    cargo run

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,9 @@ pub mod instruction;
 pub mod repl;
 pub mod vm;
 
-fn main() {}
+fn main() {
+    let mut repl = repl::REPL::new();
+    repl.run();
+    // TODO https://blog.subnetzero.io/post/building-language-vm-part-06/
+    // Command History
+}
