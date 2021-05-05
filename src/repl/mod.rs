4bf665f3152e0504956478728f287d400106fad4
Reza Handzalah <reza.handzalah@mbiz.co.id>
2021-05-05 16:06:47 +0700

    add repl history command

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -25,10 +25,19 @@ impl REPL {
                 .read_line(&mut buffer)
                 .expect("Unable to read line from user");
 
-            match buffer.trim() {
+            let buffer = buffer.trim();
+            self.command_buffer.push(buffer.to_string());
+
+            match buffer {
                 ".quit" => {
                     println!("exiting");
                     std::process::exit(0);
+                }
+                ".history" => {
+                    &self
+                        .command_buffer
+                        .iter()
+                        .for_each(|command| println!("{}", command));
                 }
                 _ => {
                     println!("Invalid input")
